60b1001ba8bd632d70fd845f4421809ce163031d
14sugunaece <14sugunaece@gmail.com>
2025-02-13 18:31:19 +0000

    Fixed cookies file location

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,13 @@
 /**Handling cookies pop-up */
 Cypress.Commands.add("saveCookies", () => {
-    cy.getCookies().then((cookies) => {
-      cy.writeFile("cypress/cookies.json", cookies);
-    });
+  cy.getCookies().then((cookies) => {
+    cy.writeFile("cypress/fixtures/cookies.json", cookies);
   });
-  
-  Cypress.Commands.add("acceptCookies", () => {
-    cy.get("reddit-cookie-banner", { includeShadowDom: true })
-      .find("faceplate-dialog", { includeShadowDom: true })
-      .find("#accept-all-cookies-button")
-      .click();
-  });
-  
+});
+
+Cypress.Commands.add("acceptCookies", () => {
+  cy.get("reddit-cookie-banner", { includeShadowDom: true })
+    .find("faceplate-dialog", { includeShadowDom: true })
+    .find("#accept-all-cookies-button")
+    .click();
+});
